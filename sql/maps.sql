abc76c2f01ded0fbdec1ca7841129d206ffd96ee
Robin Houston <robin.houston@gmail.com>
2012-03-27 15:41:18 +0100

    Snap countries to grid
    Include a snap-to-grid step in the instructions for loading country
    paths, because the paths are clearly derived from data in which they
    were snapped to a 1e-6 grid, but the points have diverged fractionally
    from this grid in incompatible ways -- so for example on the border
    between France and Switzerland, the points are ever so slightly different
    in the FR polygon and the CH polygon, which makes it impossible to
    detect borders using ST_Touches() or any other such test that relies
    on exact equality of corresponding points.

--- sql/maps.sql
+++ sql/maps.sql
@@ -7,6 +7,17 @@ insert into region (division_id, name, the_geom, area) (
     select currval('division_id_seq'), iso2, the_geom, ST_Area(the_geom) from country
 );
 
+-- All the vertices are *very* close to grid-points on a 1E-6 grid,
+-- so presumably were snapped to this grid at some point but have
+-- drifted slightly due to accumulated errors of some sort when the
+-- shape files were being prepared. Snapping back to the grid helps the
+-- borders of neighbouring countries to match precisely where they should.
+update region
+set the_geom = ST_SnapToGrid(the_geom, 1E-6)
+  , area = ST_Area(ST_SnapToGrid(the_geom, 1E-6))
+where division_id = currval('division_id_seq');
+
+
 insert into map (
   division_id,
   name, srid,
