2993bf9f14e2445cc1abc6264501043ecfd18e5f
Robin Houston <robin.houston@gmail.com>
2012-03-29 00:10:52 +0100

    New options for as-svg.py
    New options:
      --exclude-regions
      --output-grid
      --classes
      --style

--- bin/as-svg.py
+++ bin/as-svg.py
@@ -2,6 +2,7 @@
 
 import json
 import optparse
+import re
 import shlex
 import sys
 
@@ -24,6 +25,11 @@ class AsSVG(object):
       self.out = open(options.output, 'w')
     else:
       self.out = sys.stdout
+    
+    if options.exclude_regions:
+      self.exclude_regions = set(shlex.split(options.exclude_regions))
+    else:
+      self.exclude_regions = set()
 
   def print_robinson_path(self):
     c = self.db.cursor()
@@ -69,6 +75,14 @@ class AsSVG(object):
     else:
       return q(self.options.simplification)
   
+  def _transform(self, x, y):
+    if not self.options.output_grid:
+      return x, -y
+    return (
+      (x - self.m.x_min) * self.options.output_grid_width / (self.m.x_max - self.m.x_min),
+      self.options.output_grid_height - (y - self.m.y_min) * self.options.output_grid_height / (self.m.y_max - self.m.y_min),
+    )
+  
   def region_paths(self):
     c = self.db.cursor()
     try:
@@ -103,6 +117,8 @@ class AsSVG(object):
         })
       
       for region_name, g, has_data in c.fetchall():
+        if region_name in self.exclude_regions:
+          continue
         p = shapely.wkb.loads(str(g))
         yield region_name, p, has_data
     
@@ -112,12 +128,17 @@ class AsSVG(object):
   def print_region_paths(self):
     for region_name, p, has_data in self.region_paths():
       region_key = region_name # XXXX only works if the region name is a valid id
-      classes = "has-data" if has_data else "no-data"
+      if self.options.classes:
+        classes = shlex.split(self.options.classes)
+      else:
+        classes = []
+      if self.options.dataset:
+        classes += ["has-data"] if has_data else ["no-data"]
       
       if self.f is None or self.options.static:
         path = self.multipolygon_as_svg(p, self.f)
         if path:
-          print >>self.out, '<path id="{region_key}" d="{path}" class="{classes}"/>'.format(region_key=region_key, path=path, classes=classes)
+          print >>self.out, '<path id="{region_key}" d="{path}" class="{classes}"/>'.format(region_key=region_key, path=path, classes=" ".join(classes))
       else:
         original_path = self.multipolygon_as_svg(p)
         if original_path:
@@ -125,7 +146,7 @@ class AsSVG(object):
           print >>self.out, """<path id="{region_key}" d="{original}" class="{classes}">
             <animate dur="10s" repeatCount="indefinite" attributeName="d" 
                 values="{original};{morphed};{morphed};{original};{original}"/>
-          </path>""".format(region_key=region_key, original=original_path, morphed=morphed_path, classes=classes)
+          </path>""".format(region_key=region_key, original=original_path, morphed=morphed_path, classes=" ".join(classes))
   
   def print_region_paths_json(self):
     d = {}
@@ -140,8 +161,9 @@ class AsSVG(object):
       for x, y in ring.coords:
         if f:
           x, y = f(x, y)
+        x, y = self._transform(x, y)
         poly_arr.append("%.0f" % x)
-        poly_arr.append("%.0f" % -y)
+        poly_arr.append("%.0f" % y)
         if first:
           poly_arr.append("L")
           first = False
@@ -186,23 +208,38 @@ class AsSVG(object):
     c.close()
 
   def print_document(self):
+    if self.options.output_grid:
+      x_min = 0
+      minus_y_max = 0
+      x_extent = self.options.output_grid_width
+      y_extent = self.options.output_grid_height
+    else:
+      x_min = self.m.x_min
+      minus_y_max = -self.m.y_max
+      x_extent = self.m.x_max-self.m.x_min
+      y_extent = self.m.y_max-self.m.y_min
+    
+    if self.options.style:
+      external_stylesheet = open(self.options.style, 'r').read()
+    else:
+      external_stylesheet = ""
+    
     print >>self.out, """<?xml version="1.0" encoding="UTF-8"?>
   <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="%(width)d" height="%(height)d" viewBox="%(x_min).5f %(minus_y_max).5f %(x_extent).5f %(y_extent).5f">
     <style type="text/css">
-      svg { background: #eee; }
-      #robinson { fill: #9ec7f3; stroke: #999; stroke-width: 40000; }
-      #bounds { fill: #9ec7f3; stroke: #9ec7f3; }
-      path { fill: #f7d3aa; stroke: #a08070; stroke-width: %(stroke_width)d; }
+      path { fill: none; stroke: #a08070; stroke-width: %(stroke_width)s; }
       path.no-data { fill: white; }
       circle { fill: red; opacity: %(circle_opacity)f; }
+      %(external_stylesheet)s
     </style>
     <path id="bounds" d="M %(x_min).5f %(minus_y_max).5f h %(x_extent).5f v %(y_extent).5f h -%(x_extent).5f Z"/>
     """ % {
       "width": self.m.width, "height": self.m.height,
-      "x_min": self.m.x_min, "minus_y_max": -self.m.y_max,
-      "x_extent": self.m.x_max-self.m.x_min, "y_extent": self.m.y_max-self.m.y_min,
+      "x_min": x_min, "minus_y_max": minus_y_max,
+      "x_extent": x_extent, "y_extent": y_extent,
       "stroke_width": self.options.stroke_width,
-      "circle_opacity": self.options.circle_opacity
+      "circle_opacity": self.options.circle_opacity,
+      "external_stylesheet": external_stylesheet,
     }
     
     if self.options.robinson:
@@ -248,10 +285,23 @@ def main():
   parser.add_option("", "--simplification-json",
                     action="store",
                     help="A JSON-encoded dict of region name => simplification")
+  parser.add_option("", "--exclude-regions",
+                    action="store",
+                    help="Regions to exclude. Space-separated (shell-quoted)")
   
+  parser.add_option("", "--output-grid",
+                    action="store",
+                    help="the output grid, in the form <width>x<height>")
   parser.add_option("", "--stroke-width",
-                    action="store", default=2000, type="int",
+                    action="store", default=2000,
                     help="width of SVG strokes (default %default)")
+  
+  parser.add_option("", "--classes",
+                    action="store",
+                    help="CSS classes to add to countries, optionally")
+  parser.add_option("", "--style",
+                    action="store",
+                    help="a stylesheet to embed inline, optionally")
 
   parser.add_option("", "--robinson",
                     action="store_true", default=False,
@@ -283,10 +333,18 @@ def main():
       parser.error("--static doesn't make sense in JSON mode: JSON output is always static")
     
     # Not all options are yet supported in JSON output mode
+    # (Bear in mind as-js.py as an alternative, if you want output for Javascript)
     if options.circles:
       parser.error("--circles is not yet supported in JSON output mode")
     if options.robinson:
       parser.error("--robinson is not yet supported in JSON output mode")
+  
+  if options.output_grid:
+    mo = re.match(r"^(\d+)x(\d+)$", options.output_grid)
+    if mo is None:
+      parser.error("Unrecognised value for --output-grid: " + options.output_grid)
+    setattr(options, "output_grid_width", int(mo.group(1)))
+    setattr(options, "output_grid_height", int(mo.group(2)))
   
   as_svg = AsSVG(options=options)
   if options.json:
